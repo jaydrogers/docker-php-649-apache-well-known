7c376946ea322eadfcaeae47eefca2acdfc07387
Jay Rogers <jaydrogers@users.noreply.serversideup.net>
2026-02-05 15:36:25 -0600

    Update .well-known route to return OpenID configuration in JSON format

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/.well-known/', function () {
-    return view('welcome');
+Route::get('/.well-known/openid-configuration', function () {
+    $appUrl = config('app.url');
+
+    return response()->json([
+        'issuer' => $appUrl,
+        'authorization_endpoint' => $appUrl . '/oauth/authorize',
+        'token_endpoint' => $appUrl . '/oauth/token',
+        'userinfo_endpoint' => $appUrl . '/oauth/userinfo',
+        'jwks_uri' => $appUrl . '/.well-known/jwks.json',
+    ]);
 });
